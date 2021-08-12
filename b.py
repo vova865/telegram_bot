4a92fcc7fa91e1d60bb9d76fc80795fc44d78339
vova865 <84843283+vova865@users.noreply.github.com>
2021-08-12 10:06:34 +0300

    Update b.py

--- b.py
+++ b.py
@@ -1,23 +1,45 @@
 import telebot
+import yfinance as yf
 
 bot = telebot.TeleBot('1938846595:AAGungBrhrHpmGAzH-Tq72sCa1kEWyOeYsc')
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}', reply_markup=keyboard1)
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['привет'])
 def get_text_messages(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Привет!')
-    else:
-        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+    bot.send_message(message.from_user.id, 'Привет!')
 
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup()  # функция вызывает клавиатуру
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('Premier League', 'LaLiga', 'Bundesliga', 'Serie A', 'Ligue1')
+@bot.message_handler(commands=['stk'])
+def get_stocks(message):
+    response = ""
+    stocks = ['amzn', 'vod', 'ko']
+    stock_data = []
+    for stock in stocks:
+        data = yf.download(tickers=stock, period='2d', interval='1d')
+        data = data.reset_index()
+        response += f"-----{stock}-----\n"
+        stock_data.append([stock])
+        columns = ['stock']
+        for index, row in data.iterrows():
+            stock_position = len(stock_data) - 1
+            price = round(row['Close'], 2)
+            format_date = row['Date'].strftime('%m/%d')
+            response += f"{format_date}: {price}\n"
+            stock_data[stock_position].append(price)
+            columns.append(format_date)
+        print()
+
+    response = f"{columns[0] : <10}{columns[1] : ^10}{columns[2] : >10}\n"
+    for row in stock_data:
+        response += f"{row[0] : <10}{row[1] : ^10}{row[2] : >10}\n"
+    response += "\nStock Data"
+    print(response)
+    bot.send_message(message.chat.id, response)
+
 
 bot.polling()
